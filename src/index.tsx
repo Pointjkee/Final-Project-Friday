0c969dc9689d316314de10e7368a5a60edd0ac7b
Denis <razoranet@gmail.com>
2021-12-09 18:57:28 +0300

    delete idea, add provider, HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
+            <Provider store={store}>
             <App/>
-        </BrowserRouter>
+            </Provider>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
