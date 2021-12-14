268ff3f42a5e3e11779034c2a4cd740af9b3ec13
Denis <razoranet@gmail.com>
2021-12-14 15:47:16 +0300

    add material ui

--- src/component/register/Register.tsx
+++ src/component/register/Register.tsx
@@ -1,27 +1,30 @@
-import SuperInputText from "../../common/superInputText/SuperInputText";
-import React, {useState} from "react";
-import SuperButton from "../../common/superButton/SuperButton";
+import React, {ChangeEvent, useState} from "react";
 import styles from './Register.module.css'
 import {registerThunk} from "../../reducers/registerReducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../store/store";
 import {Link, Navigate} from "react-router-dom";
+import {Container, IconButton, InputAdornment, TextField} from "@mui/material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import Button from "@mui/material/Button";
 
 export const Register = React.memo(() => {
     const [email, setEmail] = useState<string>('')
     const [password1, setPassword1] = useState<string>('')
     const [password2, setPassword2] = useState<string>('')
+    const [pass1, setPass1] = useState<boolean>(false)
+    const [pass2, setPass2] = useState<boolean>(false)
     const dispatch = useDispatch()
     const errorRegister = useSelector<AppRootStateType, string | null>(state => state.register.error)
     const isRegister = useSelector<AppRootStateType, boolean>(state => state.register.isRegister)
-    const onChangeEmail = (value: string) => {
-        setEmail(value)
+    const onChangeEmail = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setEmail(e.currentTarget.value)
     }
-    const onChangePassword1 = (value: string) => {
-        setPassword1(value)
+    const onChangePassword1 = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setPassword1(e.currentTarget.value)
     }
-    const onChangePassword2 = (value: string) => {
-        setPassword2(value)
+    const onChangePassword2 = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setPassword2(e.currentTarget.value)
     }
     const onChange = () => {
         if (password1 === password2) {
@@ -35,34 +38,98 @@ export const Register = React.memo(() => {
     if (isRegister) {
         return <Navigate to='/login'/>
     }
+    const togglePassword1 = () => {
+        setPass1(!pass1)
+    }
+    const togglePassword2 = () => {
+        setPass2(!pass2)
+    }
     return (
-        <div>
-            <h1>It-incubator</h1>
-            <h3>Sign Up</h3>
-            <div>
-                <div className={styles.inputDiv}>Email:</div>
-                <div><SuperInputText onChangeText={onChangeEmail}/></div>
-            </div>
-            <div>
-                <div className={styles.inputDiv}>Password:</div>
-                <div><SuperInputText onChangeText={onChangePassword1} styleInput={'password'}/></div>
-            </div>
-            <div>
-                <div className={styles.inputDiv}>Confirm password:</div>
-                <div><SuperInputText onChangeText={onChangePassword2} styleInput={'password'}/></div>
-            </div>
-            {errorPassword
-            &&
-            <div style={{color: 'red'}}> Incorrect passwords!</div>
-            ||
-            !!errorRegister
-            &&
-            <div style={{color: 'red'}}>{errorRegister}</div>
-            }
-            <div>
-                <SuperButton onClick={onChange}>Register</SuperButton>
-                <Link to="/login"><SuperButton>Cancel</SuperButton></Link>
-            </div>
+        <div className={styles.main}>
+            <Container
+                className={styles.container}
+                sx={{width: 400, paddingBottom: 5, marginTop:3}}
+            >
+                <h1>It-incubator</h1>
+                <h3>Sign Up</h3>
+                <div>
+                    <TextField
+                        id="outlined-name"
+                        label="Email"
+                        value={email}
+                        onChange={onChangeEmail}
+                        sx={{width: 300}}
+                        size='small'
+                    />
+                </div>
+                <div style={{marginTop: 20}}>
+                    <TextField
+                        type={pass1 ? 'text' : 'password'}
+                        label='Password'
+                        size='small'
+                        sx={{width: 300}}
+                        value={password1}
+                        onChange={onChangePassword1}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={togglePassword1}
+                                        edge="end"
+                                    >
+                                        {pass1 ? <VisibilityOff/> : <Visibility/>}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                </div>
+                <div>
+                    <div style={{marginTop: 20}}>
+                        <TextField
+                            type={pass2 ? 'text' : 'password'}
+                            label='Confirm Password'
+                            size='small'
+                            sx={{width: 300}}
+                            value={password2}
+                            onChange={onChangePassword2}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={togglePassword2}
+                                            edge="end"
+                                        >
+                                            {pass2 ? <VisibilityOff/> : <Visibility/>}
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </div>
+                </div>
+                {errorPassword
+                &&
+                <div style={{color: 'red'}}> Incorrect passwords!</div>
+                ||
+                !!errorRegister
+                &&
+                <div style={{color: 'red'}}>{errorRegister}</div>
+                }
+                <div style={{marginTop: 30}}>
+                    <Button
+                        size='medium'
+                        onClick={onChange}
+                        variant="contained"
+                        style={{marginRight: 50}}
+                    >Register</Button>
+                    <Link to="/login" style={{textDecoration: 'none'}}>
+                        <Button size='medium' variant="outlined">Cancel</Button>
+                    </Link>
+                </div>
+            </Container>
         </div>
     )
 })
