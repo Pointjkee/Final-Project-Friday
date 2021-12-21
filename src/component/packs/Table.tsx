64df6c46424ce2590716cf965dc7d8ea7f1ac5f2
Denis <razoranet@gmail.com>
2021-12-21 16:35:44 +0300

    add table for packs

---
+++ src/component/packs/Table.tsx
@@ -0,0 +1,197 @@
+import * as React from 'react';
+import {useTheme} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import TableHead from "@mui/material/TableHead";
+import {useState} from "react";
+
+interface TablePaginationActionsProps {
+    count: number;
+    page: number;
+    rowsPerPage: number;
+    onPageChange: (
+        event: React.MouseEvent<HTMLButtonElement>,
+        newPage: number,
+    ) => void;
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+    const theme = useTheme();
+    const {count, page, rowsPerPage, onPageChange} = props;
+
+    const handleFirstPageButtonClick = (
+        event: React.MouseEvent<HTMLButtonElement>,
+    ) => {
+        onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <Box sx={{flexShrink: 0, ml: 2.5}}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="first page"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
+            </IconButton>
+            <IconButton
+                onClick={handleBackButtonClick}
+                disabled={page === 0}
+                aria-label="previous page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="last page"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
+            </IconButton>
+        </Box>
+    );
+}
+
+function createData(name: string, cardsCount: number, updated: string, actions: string) {
+    return {name, cardsCount, updated, actions};
+}
+
+const rows = [
+    createData('test product name', 2, '21/12/21', 'add'),
+    createData('test product name', 5, '21/12/21', 'add'),
+    createData('test product name', 6, '21/12/21', 'add'),
+    createData('test product name', 0, '21/12/21', 'add'),
+    createData('test product name', 1, '21/12/21', 'add'),
+    createData('test product name', 1, '21/12/21', 'add'),
+    createData('test product name', 2, '21/12/21', 'add'),
+    createData('test product name', 5, '21/12/21', 'add'),
+    createData('test product name', 1, '21/12/21', 'add'),
+    createData('test product name', 1, '21/12/21', 'add'),
+    createData('test product name', 0, '21/12/21', 'add'),
+    createData('test product name', 6, '21/12/21', 'add'),
+    createData('test product name', 1, '21/12/21', 'add'),
+]
+// .sort((a, b) => (a.cardsCount < b.cardsCount ? -1 : 1));
+
+export default function BasicTable() {
+    /*const sortUp = (array: any) => {
+        return array(array.sort((a: any, b: any) => (a.cardsCount < b.cardsCount ? -1 : 1)))
+    }*/
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 500}} aria-label="custom pagination table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell sx={{width: 150}}>Name</TableCell>
+                        <TableCell align="right">
+                            Cards count
+                            {<button onClick={() => {
+                                console.log('UP')
+                            }}>UP</button>}
+                            {<button onClick={() => {
+                                console.log('DOWN')
+                            }}>DOWN</button>}
+                        </TableCell>
+                        <TableCell align="right">Updated</TableCell>
+                        <TableCell align="right">Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {(rowsPerPage > 0
+                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : rows
+                    ).map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell style={{width: 160}} align="right">
+                                {row.cardsCount}
+                            </TableCell>
+                            <TableCell style={{width: 160}} align="right">
+                                {row.updated}
+                            </TableCell>
+                            <TableCell style={{width: 160}} align="right">
+                                {row.actions}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                    {emptyRows > 0 && (
+                        <TableRow style={{height: 53 * emptyRows}}>
+                            <TableCell colSpan={6}/>
+                        </TableRow>
+                    )}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
+                            colSpan={3}
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            SelectProps={{
+                                inputProps: {
+                                    'aria-label': 'rows per page',
+                                },
+                                native: true,
+                            }}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                            ActionsComponent={TablePaginationActions}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+        </TableContainer>
+    );
+}
