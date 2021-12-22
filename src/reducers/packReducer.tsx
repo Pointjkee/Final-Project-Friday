2d516c468e6f13b4d7a373b0070f11257e032b78
masa_i@bk.ru <Gabibula1>
2021-12-22 13:39:04 +0300

    2 commit => 2 lesson.

--- src/reducers/packReducer.tsx
+++ src/reducers/packReducer.tsx
@@ -1,17 +1,16 @@
 import {GetPackType, packAPI} from "../api/api";
-import {Dispatch} from "redux";
+import {ThunkAction} from "redux-thunk";
+import {AppRootStateType} from "../store/store";
 
 
-const initialState = {
-
-}
+const initialState = {}
 
 
 export type InitialStateTypeProfile = typeof initialState
 
 export const packReducer = (state: InitialStateTypeProfile = initialState, action: SetPackType): InitialStateTypeProfile => {
     switch (action.type) {
-        case "PACK/SET-PACK":{
+        case "PACK/SET-PACK": {
             return {...state, ...action.data}
         }
         default:
@@ -20,10 +19,54 @@ export const packReducer = (state: InitialStateTypeProfile = initialState, actio
 }
 
 type SetPackType = ReturnType<typeof setStatePack>
-export const setStatePack = (data: GetPackType) => ({type:"PACK/SET-PACK", data} as const)
+export const setStatePack = (data: GetPackType) => ({type: "PACK/SET-PACK", data} as const)
 
-export const getPack = () => (dispatch:Dispatch) => {
-    packAPI.getPack().then(res=>{
+export const getPack = (): PackThunkType => (dispatch) => {
+    packAPI.getPack().then(res => {
         dispatch(setStatePack(res.data))
     })
+}
+
+export const addPack = (data?:PostPackType): PackThunkType => (dispatch) => {
+    packAPI.postPack(data).then(() => {
+        dispatch(getPack())
+    })
+
+}
+
+export const deletePack = (id: string): PackThunkType => (dispatch) => {
+    packAPI.deletePack(id).then(() => {
+        dispatch(getPack())
+    })
+}
+
+
+//  dispatch(updatePack({cardsPack:{_id:"61c2fd76b68c5b0004438b99"} })) вот так передавать в санку))
+export const updatePack = (data: UpdatePackType): PackThunkType => (dispatch) => {
+    packAPI.updatePack(data).then(()=> {
+        dispatch(getPack())
+    })
+}
+
+export type PackThunkType = ThunkAction<void, AppRootStateType, unknown, SetPackType>
+
+
+export type UpdatePackType = {
+    cardsPack: {
+        _id: string
+        name?: string
+    }
+}
+
+export type PostPackType = {
+    cardsPack: {
+        name?: string
+        path?: string
+        grade?: number
+        shots?: number
+        rating?: number
+        deckCover?: string
+        private?: boolean
+        type?: string
+    }
 }
