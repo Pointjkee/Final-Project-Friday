6ef968b9dee1adb0f61e831709ec6fe611fc1031
Denis <razoranet@gmail.com>
2021-12-22 13:44:56 +0300

    add table ver.#2

--- src/component/packs/Packs.tsx
+++ src/component/packs/Packs.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import DataTable from './Table';
 import styles from './Packs.module.css'
-import BasicTable from './Table';
+
 
 export const Packs = () => {
     return <div className={styles.main}>
-        <div><BasicTable/></div>
+        <div><DataTable/></div>
     </div>
 }
--- src/component/packs/Table.tsx
+++ src/component/packs/Table.tsx
@@ -1,197 +1,71 @@
 import * as React from 'react';
-import {useTheme} from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
-import TableHead from "@mui/material/TableHead";
-import {useState} from "react";
+import {DataGrid, GridApi, GridCellValue, GridColDef} from '@mui/x-data-grid';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {cardPacksType, getPack} from "../../reducers/packReducer";
+import {AppRootStateType} from "../../store/store";
+import Button from '@material-ui/core/Button';
 
-interface TablePaginationActionsProps {
-    count: number;
-    page: number;
-    rowsPerPage: number;
-    onPageChange: (
-        event: React.MouseEvent<HTMLButtonElement>,
-        newPage: number,
-    ) => void;
-}
+const columns: GridColDef[] = [
+    // {field: 'id', headerName: 'ID', width: 70 },
+    {field: 'name', headerName: 'Name pack', width: 650, sortable: false, disableColumnMenu: true},
+    {
+        field: 'cardsCount',
+        headerName: 'Cards count',
+        type: 'number',
+        width: 150,
+        align: 'center',
+        disableColumnMenu: true
+    },
+    {field: 'updated', headerName: 'Updated', width: 260, disableColumnMenu: true},
+    {
+        field: 'actions',
+        headerName: 'Actions',
+        width: 140,
+        align: 'right',
+        sortable: false,
+        disableColumnMenu: true,
+        renderCell: (params) => {
+            const onClick = (e: any) => {
+                e.stopPropagation(); // don't select this row after clicking
+                const api: GridApi = params.api;
+                const thisRow: Record<string, GridCellValue> = {};
+                api
+                    .getAllColumns()
+                    .filter((c) => c.field !== '__check__' && !!c)
+                    .forEach(
+                        (c) => (thisRow[c.field] = params.getValue(params.id, c.field)),
+                    );
+                return alert(JSON.stringify(thisRow, null, 4));
+            };
+            return <Button onClick={onClick}>Click</Button>;
+        },
+    }
+];
+export default function DataTable() {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getPack())
+    }, [])
 
-function TablePaginationActions(props: TablePaginationActionsProps) {
-    const theme = useTheme();
-    const {count, page, rowsPerPage, onPageChange} = props;
+    function createData(id: string, name: string, cardsCount: number, updated: string, actions: string) {
+        return {id, name, cardsCount, updated, actions};
+    }
 
-    const handleFirstPageButtonClick = (
-        event: React.MouseEvent<HTMLButtonElement>,
-    ) => {
-        onPageChange(event, 0);
-    };
-
-    const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onPageChange(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onPageChange(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
+    const packs = useSelector<AppRootStateType, cardPacksType>(state => state.pack.cardPacks)
+    let rows = packs.map((t) => {
+        return createData(t._id, t.name, t.cardsCount, t.updated, 'add')
+    })
 
     return (
-        <Box sx={{flexShrink: 0, ml: 2.5}}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
-            </IconButton>
-            <IconButton
-                onClick={handleBackButtonClick}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
-            </IconButton>
-        </Box>
-    );
-}
-
-function createData(name: string, cardsCount: number, updated: string, actions: string) {
-    return {name, cardsCount, updated, actions};
-}
-
-const rows = [
-    createData('test product name', 2, '21/12/21', 'add'),
-    createData('test product name', 5, '21/12/21', 'add'),
-    createData('test product name', 6, '21/12/21', 'add'),
-    createData('test product name', 0, '21/12/21', 'add'),
-    createData('test product name', 1, '21/12/21', 'add'),
-    createData('test product name', 1, '21/12/21', 'add'),
-    createData('test product name', 2, '21/12/21', 'add'),
-    createData('test product name', 5, '21/12/21', 'add'),
-    createData('test product name', 1, '21/12/21', 'add'),
-    createData('test product name', 1, '21/12/21', 'add'),
-    createData('test product name', 0, '21/12/21', 'add'),
-    createData('test product name', 6, '21/12/21', 'add'),
-    createData('test product name', 1, '21/12/21', 'add'),
-]
-// .sort((a, b) => (a.cardsCount < b.cardsCount ? -1 : 1));
-
-export default function BasicTable() {
-    /*const sortUp = (array: any) => {
-        return array(array.sort((a: any, b: any) => (a.cardsCount < b.cardsCount ? -1 : 1)))
-    }*/
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-    const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-    const handleChangePage = (
-        event: React.MouseEvent<HTMLButtonElement> | null,
-        newPage: number,
-    ) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    ) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: 500}} aria-label="custom pagination table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell sx={{width: 150}}>Name</TableCell>
-                        <TableCell align="right">
-                            Cards count
-                            {<button onClick={() => {
-                                console.log('UP')
-                            }}>UP</button>}
-                            {<button onClick={() => {
-                                console.log('DOWN')
-                            }}>DOWN</button>}
-                        </TableCell>
-                        <TableCell align="right">Updated</TableCell>
-                        <TableCell align="right">Actions</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {(rowsPerPage > 0
-                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            : rows
-                    ).map((row) => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell style={{width: 160}} align="right">
-                                {row.cardsCount}
-                            </TableCell>
-                            <TableCell style={{width: 160}} align="right">
-                                {row.updated}
-                            </TableCell>
-                            <TableCell style={{width: 160}} align="right">
-                                {row.actions}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                    {emptyRows > 0 && (
-                        <TableRow style={{height: 53 * emptyRows}}>
-                            <TableCell colSpan={6}/>
-                        </TableRow>
-                    )}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
-                            colSpan={3}
-                            count={rows.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            SelectProps={{
-                                inputProps: {
-                                    'aria-label': 'rows per page',
-                                },
-                                native: true,
-                            }}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                            ActionsComponent={TablePaginationActions}
-                        />
-                    </TableRow>
-                </TableFooter>
-            </Table>
-        </TableContainer>
+        <div style={{height: 480, width: '100%'}}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={7}
+                rowsPerPageOptions={[5]}
+                checkboxSelection={false}
+            />
+        </div>
     );
 }
