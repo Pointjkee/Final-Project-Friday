fbf3c752c110958216bbb449f9b306b8a93c7bef
Denis <razoranet@gmail.com>
2021-12-24 18:00:42 +0300

    fix some bug

--- src/component/cards/card/Card.tsx
+++ src/component/cards/card/Card.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
 import {CardType} from "../../../reducers/cardReducer";
-import {Button} from "@material-ui/core";
+
 
 
 type PropsCardType = {
