2592d9ff82929bf5924cf6b500a515f058a72ecb
ArtemEfimovich <artemfender@gmail.com>
2021-12-30 17:54:50 +0300

    fix styles, add new year mood

--- src/component/packs/newPack/newTable/tableRow/tableRow.tsx
+++ src/component/packs/newPack/newTable/tableRow/tableRow.tsx
@@ -3,13 +3,9 @@ import style from './Tabble.module.css'
 import {Button} from "@material-ui/core";
 import {useSelector} from "react-redux";
 import {AppRootStateType} from "../../../../../store/store";
-import {useDispatch} from "react-redux";
-import {deletePack} from "../../../../../reducers/packReducer";
 import ModalDelete from "../../../modal/ModalDelete";
 import ModalEdit from "../../../modal/ModalEdit";
-import {NavLink} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
-import CardGame from "../../../../cardGame/CardGame";
 
 type RowPropsType = {
     name: string
@@ -30,26 +26,20 @@ let buttonWrapper = {
     justifyContent: "space-around",
 }
 
-export const Row = ({name, cards, update, createdName, cardUserId,packId,text}: RowPropsType) => {
+export const Row = ({name, cards, update, createdName, cardUserId, packId, text}: RowPropsType) => {
     const meProfileId = useSelector<AppRootStateType, string | null>(s => s.profile.profile._id)
     const navigate = useNavigate()
 
 
-        const onCardsNavigateClick = () => {
-            return navigate('/cards/' + packId)
-        }
+    const onCardsNavigateClick = () => {
+        return navigate('/cards/' + packId)
+    }
 
-        const onGameNavigateClick = () => {
-            navigate('/game/' + packId)
-        }
+    const onGameNavigateClick = () => {
+        navigate('/game/' + packId)
+    }
 
-        return (
-            <div>
-                <div className={style.rowTable}>
-                    <div className={style.rowText} style={{textAlign: "start"}}> {name} </div>
-                    <div className={style.rowText} style={{width: "30%"}}>{cards}</div>
-                    <div className={style.rowText}>{update}</div>
-                    <div className={style.rowText} style={{width: "100%"}}>{createdName}</div>
+
     return (
         <div>
             <div className={style.rowTable}>
@@ -58,26 +48,26 @@ export const Row = ({name, cards, update, createdName, cardUserId,packId,text}: 
                 <div className={style.rowText} style={{width: "100%"}}>{update}</div>
                 <div className={style.rowText}>{createdName}</div>
 
-                    <div style={buttonWrapper}>
+                <div style={buttonWrapper}>
 
-                        {cardUserId === meProfileId &&
-                        <>
-                            <ModalDelete packId={packId} text={text}/>
-                            <ModalEdit title={name} packId={packId}/>
-                        </>}
-                        {cards > 0 &&<>
-                            <Button style={{width: "30%"}} size={"small"} variant={"contained"}
-                                    color={"secondary"}
-                            onClick={onCardsNavigateClick}>Look</Button>
-                         <Button
+                    {cardUserId === meProfileId &&
+                    <>
+                        <ModalDelete packId={packId} text={text}/>
+                        <ModalEdit title={name} packId={packId}/>
+                    </>}
+                    {cards > 0 && <>
+                        <Button style={{width: "30%"}} size={"small"} variant={"contained"}
+                                color={"secondary"}
+                                onClick={onCardsNavigateClick}>Look</Button>
+                        <Button
                             style={{width: "30%"}} size={"small"} variant={"contained"}
                             color={"secondary"}
                             onClick={onGameNavigateClick}>Learn</Button>
-                        </>}
-
-                    </div>
+                    </>}
 
                 </div>
+
             </div>
-        )
-    }
+        </div>
+    )
+}
